18eca891c361f86eebc7309789d6da0c27d99c23
kam6y <daiyakanba0305@gmail.com>
2025-12-16 11:37:25 +0900

    MOのDiagramの拡大率を調整

--- src/web/components/MolecularOrbitalEnergyDiagram.tsx
+++ src/web/components/MolecularOrbitalEnergyDiagram.tsx
@@ -483,7 +483,7 @@ export const MolecularOrbitalEnergyDiagram: React.FC<MolecularOrbitalEnergyDiagr
         // Zoom behavior with translation limits
         const zoom = d3
           .zoom<SVGSVGElement, unknown>()
-          .scaleExtent([1, 100])
+          .scaleExtent([1, 200])
           .translateExtent([
             [0, 0],
             [DIAGRAM_CONFIG.width, DIAGRAM_CONFIG.height],
